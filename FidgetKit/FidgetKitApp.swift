2aaab9b2bb9c08c03ac271164f748e74c8975fb4
Jefferson Setiawan <jefferson.setiawan@icloud.com>
2020-08-11 19:30:02 +0700

    Initial Commit

---
+++ FidgetKit/FidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetKitApp.swift
+//  FidgetKit
+//
+//  Created by Jefferson Setiawan on 11/08/20.
+//
+
+import SwiftUI
+
+@main
+struct FidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
